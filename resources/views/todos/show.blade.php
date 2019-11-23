46e8a59e87827a8962484510f2b5037817d6e031
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-11-23 16:14:48 +0100

    adds jquery

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 @section('content')
-<card class="card-default">
-    <div class="card-header"> {{ $todo->name }}</div>
-    <p class="text-center card-bodys"> {{$todo->description}} </p>
-    <a class="btn btn-info" href="/edit/{{ $todo->id }}">Edit </a>
-</card>
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <card class="card-default">
+            <div class="card-header"> {{ $todo->name }}</div>
+            <p class="text-center card-bodys"> {{$todo->description}} </p>
+            <a class="btn btn-info" href="/edit/{{ $todo->id }}">Edit </a>
+        </card>
+    </div>
+</div>
 @endsection
