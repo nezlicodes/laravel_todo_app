73b7339d0d26fe2a31f1251af8b27c81a74abc7b
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-11-23 16:39:20 +0100

    tries with db connection

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => 'DB_CONNECTION', 'mysql',
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
